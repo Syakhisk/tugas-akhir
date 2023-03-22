ea7456056c549355685e0967a8c414b72e7e0dd4
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-03-22 18:54:27 +0000

    update

--- proxmox/knative-node/01-alias-kubectl.sh
+++ proxmox/knative-node/01-alias-kubectl.sh
@@ -4,3 +4,5 @@ echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
 echo '[[ ! -f $HOME/.local/bin/kubectl ]] && ln -s $(which minikube) ~/.local/bin/kubectl' >> ~/.bashrc
 echo 'source <(minikube completion bash)' >> ~/.bashrc
 echo 'source <(kubectl completion bash)' >> ~/.bashrc
+echo 'alias k="kubectl"'
+echo 'complete -o default -F __start_kubectl k'
