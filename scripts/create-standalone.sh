7ee7440a25d60cdb5614a9ece0f5d5e9607e36a6
root <root@pve.its.ac.id>
2023-04-10 20:09:39 +0700

    update create-standalone.sh script

--- scripts/create-standalone.sh
+++ scripts/create-standalone.sh
@@ -1,18 +1,18 @@
 #!/bin/env bash
 
-VM_ID="701"
-IMAGE="/root/images/resized-jammy-server-cloudimg-amd64.img"
+VM_ID="9003"
+IMAGE="/root/images/resized-jammy-server.img"
 IP="10.0.0.20/22"
 GW="10.0.0.1"
 
 qm create "$VM_ID"\
-	--name "standalone-$VM_ID" \
-	--memory 4096 \
-	--cores 4 \
+	--name "standalone" \
+	--memory 512 \
+	--cores 1 \
 	--net0 virtio,bridge=vmbr2 \
 	--scsihw virtio-scsi-pci \
 	--scsi0 local-lvm:0,import-from="$IMAGE" \
 	--ide2 local-lvm:cloudinit \
 	--boot order=scsi0 \
 	--serial0 socket --vga serial0 \
-	--ciuser ubuntu --cipassword ubuntu
+	--ciuser ubuntu --cipassword="$(openssl passwd -6 ubuntu)"
