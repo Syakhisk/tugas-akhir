c8952875be6eaca61e37b039d12bf59ad9a89e8a
root <root@pve.its.ac.id>
2023-05-10 22:51:04 +0700

    update create template script

--- scripts/create-standalone.sh
+++ scripts/create-standalone.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
+# Script to create TEMPLATE VM
+# To create clone use pve ui
+
 VM_ID="9003"
 IMAGE="/root/images/resized-jammy-server.img"
 IP="10.0.0.20/22"
@@ -15,4 +18,5 @@ qm create "$VM_ID"\
 	--ide2 local-lvm:cloudinit \
 	--boot order=scsi0 \
 	--serial0 socket --vga serial0 \
+	--ipconfig0 ip="$IP",gw="$GW" \
 	--ciuser ubuntu --cipassword="$(openssl passwd -6 ubuntu)"
