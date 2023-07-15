f321149dc10e740587e6f814cfcfcccfe035edd5
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-07-15 20:16:35 +0700

    add local setup

--- local-setup/hey-tests/index.js
+++ local-setup/hey-tests/index.js
@@ -1,6 +1,7 @@
 import { $, fs } from "zx";
 
-const host = "fibonacci-behavior.default.local.knative";
+// const host = "fibonacci-behavior.default.local.knative";
+const host = "fibonacci-behavior-smallest.default.local.knative";
 const baseURL = `http://${process.env["MINIKUBE_IP"]}:${process.env["KOURIER_PORT"]}`;
 
 const run = (count) => $`hey -n ${count} -c ${count} -host ${host} ${baseURL}`;
@@ -10,20 +11,25 @@ const datetime = new Date()
   .replace(/\/|\./g, "-")
   .replace(/ /g, "_");
 
-let start = 100;
-let increment = 100;
+// let start = 100;
+// let increment = 100;
+// let iteration = 12 * 2;
+
+let start = 200;
+let increment = 400;
 let iteration = 10;
+
+
+const filename = `results/raw-${start + increment * (iteration - 1)}-${datetime}`;
 
 for (let i = 0; i < iteration; i++) {
   const out = await run(start);
   const pods = await $`kubectl get pods --sort-by=.metadata.creationTimestamp`;
 
-  // await $`echo "${out}" > results/raw-${datetime}`;
-  // await $`echo "${out}" >> results/summary-${datetime}`;
+  fs.appendFileSync(filename, out.stdout);
+  fs.appendFileSync(filename, '\n\n---\n\n')
+  fs.appendFileSync(filename, pods.stdout);
+  fs.appendFileSync(filename, '\n\n---\n\n')
 
-  fs.appendFileSync(`results/raw-${datetime}`, out.stdout);
-  fs.appendFileSync(`results/raw-${datetime}`, '\n\n---\n\n')
-  fs.appendFileSync(`results/raw-${datetime}`, pods.stdout);
-  fs.appendFileSync(`results/raw-${datetime}`, '\n\n---\n\n')
   start += increment;
 }
--- web-services/fibonacci/index.js
+++ web-services/fibonacci/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+let handled = 0;
+
 // Fibonacci function
 function fibonacci(n) {
   if (n < 2) {
@@ -14,12 +16,14 @@ app.get("/", (req, res) => {
   const n = parseInt(30);
   const result = fibonacci(n);
   res.json({ result });
+  console.log("Req handled: ", ++handled);
 });
 
 app.get("/:n", (req, res) => {
   const n = parseInt(req.params.n);
   const result = fibonacci(n);
   res.json({ result });
+  console.log("Req handled: ", ++handled);
 });
 
 // Start server
