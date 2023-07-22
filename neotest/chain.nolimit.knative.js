8080ae37886efa408504443dd45030674fa03115
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-07-23 05:00:16 +0700

    tweak behavior pod

--- neotest/chain.nolimit.knative.js
+++ neotest/chain.nolimit.knative.js
@@ -6,8 +6,8 @@ const host = `${svc}.grogu.hosts.pve`;
 const target = $.env["TARGET"];
 const baseURL = "http://10.0.0.10";
 
-// const run = (count) => $`hey -n ${count} -c ${count} -host ${host} ${baseURL}`;
-const run = (count) => $`hey -n ${count} -c ${count < 2000 ? count : 2000} -host ${host} ${baseURL}`;
+const run = (count) => $`hey -n ${count} -c ${count} -host ${host} ${baseURL}`;
+// const run = (count) => $`hey -n ${count} -c ${count < 2000 ? count : 2000} -host ${host} ${baseURL}`;
 
 const counts = [
   1, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 4000, 6000, 8000, 10000
