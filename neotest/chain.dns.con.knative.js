5b171083e6d3a74399db71e478bbcca3d3b81b1b
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-07-23 11:24:26 +0700

    add new test script

---
+++ neotest/chain.dns.con.knative.js
@@ -0,0 +1,27 @@
+import { $, fs } from "zx";
+
+const svc = "fibonacci-autoscaling-con";
+const host = `${svc}.grogu.hosts.pve`;
+
+const target = $.env["TARGET"];
+const baseURL = `https://${host}`;
+
+const run = (count) => $`hey -n ${count} -c ${count < 2000 ? count : 2000} ${baseURL}`;
+
+const counts = [
+  1, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000, 4000, 6000, 8000, 10000
+];
+
+const { stdout: scriptname } = await $`basename "${process.argv[1]}" | awk -F '.js' '{print $1}'`;
+const { stdout: datetime } = await $`TZ='Asia/Jakarta' date +%Y-%m-%d_%H-%M-%S`;
+
+const filename = `results/${scriptname}-${target}-${counts[0]}-${
+  counts[counts.length - 1]
+}-${datetime}`;
+
+for (let count of counts) {
+  const out = await run(count);
+
+  fs.appendFileSync(filename, out.stdout);
+  fs.appendFileSync(filename, "\n\n---\n\n");
+}
