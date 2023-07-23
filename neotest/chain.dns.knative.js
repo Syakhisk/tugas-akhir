34bc501ef8e27c2ce4ea770c82c9920779a85783
Syakhisk Al-Azmi <mail.syakhisk@gmail.com>
2023-07-23 14:40:05 +0700

    change script

--- neotest/chain.dns.knative.js
+++ neotest/chain.dns.knative.js
@@ -15,13 +15,16 @@ const counts = [
 const { stdout: scriptname } = await $`basename "${process.argv[1]}" | awk -F '.js' '{print $1}'`;
 const { stdout: datetime } = await $`TZ='Asia/Jakarta' date +%Y-%m-%d_%H-%M-%S`;
 
-const filename = `results/${scriptname}-${target}-${counts[0]}-${
+let filename = `results/${scriptname}-${target}-${counts[0]}-${
   counts[counts.length - 1]
 }-${datetime}`;
+
+filename = filename.replace(/\n/g, "");
 
 for (let count of counts) {
   const out = await run(count);
 
+  fs.appendFileSync(filename, `## ${count} Req\n`);
   fs.appendFileSync(filename, out.stdout);
   fs.appendFileSync(filename, "\n\n---\n\n");
 }
